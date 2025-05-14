40ab5d50e5d013fdde0a3247e0b0d7e8b5b94a6a
DivanFerrari <104350966+DivanFerrari@users.noreply.github.com>
2025-05-14 16:00:20 +0200

    Added the Frontend to Project Solution (AgriEnergyFrontend)

---
+++ AgriEnergyFrontend/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AgriEnergyFrontend.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
